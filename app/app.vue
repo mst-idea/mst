8a4566b035be5b2742aed1c51ae087e2edb35d65
James Aprosail <aprosail@outlook.com>
2025-08-11 08:46:19 +0800

    feat: titlebar basic layout

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,31 @@
 <template>
-  <NuxtPage />
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
+
+<style>
+  :root {
+    --foreground-color: #232323;
+    --background-color: #fefdfa;
+    --title-bar-color: #fefdfa;
+    --border-color: #e1e1e1;
+  }
+
+  body {
+    margin: 0;
+    height: 100vh;
+    overflow: hidden;
+    user-select: none;
+
+    background-color: var(--background-color);
+    color: var(--foreground-color);
+    font-size: 16;
+    font-family: sans-serif;
+  }
+
+  div#__nuxt {
+    height: 100%;
+    width: 100%;
+  }
+</style>
